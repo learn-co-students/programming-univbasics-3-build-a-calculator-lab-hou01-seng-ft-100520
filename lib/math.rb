fbb414f9fe43f4277caafd12d480ab2788d98dd9
Crystal T <ctalley.cat@gmail.com>
2020-09-22 23:10:46 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -9,7 +9,7 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  return num2 / num1 
+  return num1 / num2 
 
 end
 
@@ -24,6 +24,6 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-  return num ** 
+  return Math.sqrt num 
 
 end
